6285d11726a58259323c78f11c202d12210b8d51
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2023-05-16 15:02:52 +0700

    feat: add colour

--- queue.py
+++ queue.py
@@ -6,26 +6,26 @@ class Queue:
     def __init__(self):
         self.q = list()
 
-    def queue_push(self, x):
-        self.q.append(x)
-
-    def queue_pop(self):
-        if len(self.q) == 0:
-            print('queue is empty')
-            return None
-        else:
-            del (self.q[0])
-            return None
-
-    def isempty(self):
+    def queue_isempty(self):
         if len(self.q) == 0:
             return True
         else:
             return False
 
+    def queue_push(self, x):
+        self.q.append(x)
+
+    def queue_pop(self):
+        if len(self.q) == 0:
+            print('Empty queue')
+            return None
+        else:
+            del (self.q[0])
+            return None
+
     def front(self):
         if len(self.q) != 0:
             return self.q[0]
         else:
-            print('queue is empty')
+            print('Empty Queue')
             return None
